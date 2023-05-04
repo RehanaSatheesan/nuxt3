8bc67821e84572adf735e8cfbff85777bf796ac2
Rehana S <rehana@techgentsia.com>
2023-05-04 17:38:30 +0530

    feat: add config files

---
+++ pages/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>Check developer console!</div>
+  </div>
+</template>
+<script setup lang="ts">
+const config = useRuntimeConfig();
+const appConfig = useAppConfig();
+console.log(appConfig.theme.primaryColor, "appConfig.theme");
+// console.log("Runtime config:", config);
+// console.log(config.apiSecret, "runtimeConfig.apiSecret");
+// console.log(config.public.apiBase, "runtimeConfig.public.apiBase");
+if (process.server) {
+  console.log("API secret:", config.apiSecret);
+}
+</script>
